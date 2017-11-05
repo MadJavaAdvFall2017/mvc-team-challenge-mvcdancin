b14309666ac8ef8c8654f60d4294d8d6038e2620
Student <agoldbin@madisoncollege.edu>
2017-11-05 16:03:14 -0600

    impersonating aaron

--- src/java112/project3/Doggo.java
+++ src/java112/project3/Doggo.java
@@ -7,92 +7,94 @@ package java112.project3;
 */
 public class Doggo extends java.lang.Object {
 
-   private String doggoTypo;
-   private String doggoHairType;
-   private int doggoAverageWeight;
-   private int doggoAverageLifespan;
+    private String doggoType;
+    private String doggoHairType;
+    private int doggoAverageWeight;
+    private int doggoAverageLifespan;
+       
+    Doggo doggo = new Doggo();
 
-   /**
+    /**
     *  Constructor for the Doggo object
     */
-   public Doggo() {
+    public Doggo() {
        doggoType = "A friendly mutt";
        doggoHairType = "Coarse doggo hairs";
-       doggoAverageWeight = 33.5;
+       doggoAverageWeight = 33;
        doggoAverageLifespan = 100;
-   }
-
-
-   /**
+    }
+    
+    
+    /**
     *  Gets the doggoType attribute of the Doggo object
     *
     *@return    The doggoType value
     */
-   public String getDoggoType() {
+    public String getDoggoType() {
        return doggoType;
-   }
-
-
-   /**
+    }
+    
+    
+    /**
     *  Sets the doggoType attribute of the Doggo object
     *
     *@param  doggoType  The new doggoType value
     */
-   public void setDoggoType(String doggoType) {
-       this.mySpecialData = mySpecialData;
-   }
-   
-   /**
+    public void setDoggoType(String doggoType) {
+       this.doggoType = doggoType;
+    }
+    
+    /**
     *  Gets the doggoHairType attribute of the Doggo object
     *
     *@return    The doggoHairType value
     */
-   public String getDoggoHairType() {
+    public String getDoggoHairType() {
        return doggoHairType;
-   }
-
-   /**
+    }
+    
+    /**
     *  Sets the doggoHairType attribute of the Doggo object
     *
     *@param  doggoHairType  The new doggoHairType value
     */
-   public void setDoggoHairType(String doggoHairType) {
-       this.mySpecialData = mySpecialData;
-   }
-   
-   /**
+    public void setDoggoHairType(String doggoHairType) {
+       this.doggoHairType = doggoHairType;
+    }
+    
+    /**
     *  Gets the doggoAverageWeight attribute of the Doggo object
     *
     *@return    The doggoAverageWeight value
     */
-   public String getDoggoAverageWeight() {
+    public int getDoggoAverageWeight() {
        return doggoAverageWeight;
-   }
-
-   /**
+    }
+    
+    /**
     *  Sets the doggoAverageWeight attribute of the Doggo object
     *
     *@param  doggoAverageWeight  The new doggoAverageWeight value
     */
-   public void setDoggoAverageWeight(String doggoAverageWeight) {
-       this.mySpecialData = mySpecialData;
-   }
-   
+    public void setDoggoAverageWeight(int doggoAverageWeight) {
+       this.doggoAverageWeight = doggoAverageWeight;
+    }
+    
     /**
     *  Gets the doggoAverageLifespan attribute of the Doggo object
     *
     *@return    The doggoAverageLifespan value
     */
-   public String getDoggoAverageLifespan() {
+    public int getDoggoAverageLifespan() {
        return doggoAverageLifespan;
-   }
-
-   /**
+    }
+    
+    /**
     *  Sets the doggoAverageLifespan attribute of the Doggo object
     *
     *@param  doggoAverageLifespan  The new doggoAverageLifespan value
     */
-   public void setDoggoAverageLifespan(String doggoAverageLifespan) {
-       this.mySpecialData = mySpecialData;
-   }
+    public void setDoggoAverageLifespan(int doggoAverageLifespan) {
+       this.doggoAverageLifespan = doggoAverageLifespan;
+    }
 }

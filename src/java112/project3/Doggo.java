a5ba1839b129127deee92bb11b38ad32bd8c42c3
Nicole Steck <steck.n@gmail.com>
2017-11-05 14:43:46 -0600

    test

---
+++ src/java112/project3/Doggo.java
@@ -0,0 +1,98 @@
+package java112.project3;
+
+/**
+*  This JavaBean is for identifying pretty pretty doggos.
+*
+*@author    nsteck
+*/
+public class Doggo extends java.lang.Object {
+
+   private String doggoTypo;
+   private String doggoHairType;
+   private int doggoAverageWeight;
+   private int doggoAverageLifespan;
+
+   /**
+    *  Constructor for the Doggo object
+    */
+   public Doggo() {
+       doggoType = "A friendly mutt";
+       doggoHairType = "Coarse doggo hairs";
+       doggoAverageWeight = 33.5;
+       doggoAverageLifespan = 100;
+   }
+
+
+   /**
+    *  Gets the doggoType attribute of the Doggo object
+    *
+    *@return    The doggoType value
+    */
+   public String getDoggoType() {
+       return doggoType;
+   }
+
+
+   /**
+    *  Sets the doggoType attribute of the Doggo object
+    *
+    *@param  doggoType  The new doggoType value
+    */
+   public void setDoggoType(String doggoType) {
+       this.mySpecialData = mySpecialData;
+   }
+   
+   /**
+    *  Gets the doggoHairType attribute of the Doggo object
+    *
+    *@return    The doggoHairType value
+    */
+   public String getDoggoHairType() {
+       return doggoHairType;
+   }
+
+   /**
+    *  Sets the doggoHairType attribute of the Doggo object
+    *
+    *@param  doggoHairType  The new doggoHairType value
+    */
+   public void setDoggoHairType(String doggoHairType) {
+       this.mySpecialData = mySpecialData;
+   }
+   
+   /**
+    *  Gets the doggoAverageWeight attribute of the Doggo object
+    *
+    *@return    The doggoAverageWeight value
+    */
+   public String getDoggoAverageWeight() {
+       return doggoAverageWeight;
+   }
+
+   /**
+    *  Sets the doggoAverageWeight attribute of the Doggo object
+    *
+    *@param  doggoAverageWeight  The new doggoAverageWeight value
+    */
+   public void setDoggoAverageWeight(String doggoAverageWeight) {
+       this.mySpecialData = mySpecialData;
+   }
+   
+    /**
+    *  Gets the doggoAverageLifespan attribute of the Doggo object
+    *
+    *@return    The doggoAverageLifespan value
+    */
+   public String getDoggoAverageLifespan() {
+       return doggoAverageLifespan;
+   }
+
+   /**
+    *  Sets the doggoAverageLifespan attribute of the Doggo object
+    *
+    *@param  doggoAverageLifespan  The new doggoAverageLifespan value
+    */
+   public void setDoggoAverageLifespan(String doggoAverageLifespan) {
+       this.mySpecialData = mySpecialData;
+   }
+}
